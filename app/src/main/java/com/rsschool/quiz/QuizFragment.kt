d76801e7f48bcc5b1109566f9ad0c36c963ef164
OrLove101 <danyazerorebel@gmail.com>
2021-06-14 19:15:47 +0300

    fix: replace _binding = null in onDestroyView

--- app/src/main/java/com/rsschool/quiz/QuizFragment.kt
+++ app/src/main/java/com/rsschool/quiz/QuizFragment.kt
@@ -113,8 +113,8 @@ class QuizFragment: Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 
